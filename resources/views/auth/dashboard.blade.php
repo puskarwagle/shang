1c79db1282f959cb853c5e8a105827e50781f5cf
puskarwagle <puskarwagle17@gmail.com>
2023-05-22 16:29:12 +0545

    made a new visual cms

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -8,137 +8,15 @@
 
 <div class="dashboard">
   <h1>CONTENT MANAGEMENT SYSTEM</h1><br>
+    
+</div> 
+    
+    @include('cms.recentWorks.modify')
 
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Overview section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.overviews.index')
-  </div>
-  
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Our clients section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.ourClients.index')
-  </div>
-
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Header Products section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.headerProducts.index')
-  </div>
-
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Header Service section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.headerServices.index')
-  </div>
-  
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Our Services section cms.</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.service.index')
-  </div>
-
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Recent Works section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.recentWorks.index')
-  </div>
-
-  <div class="dashboardDivs">
-    <h2 class="dashboardDivsH2">
-      <span>Explore our technology section cms</span>
-      <i class="fas fa-chevron-down fa-xs"></i>
-    </h2>
-      @include('cms.exploreTechs.index')
-  </div>
-
-</div> <!-- class="dashboard" -->
-
-<script>
-  const cmsDivs = document.querySelectorAll('.cmsDivs');
-
-  cmsDivs.forEach(cmsDiv => {
-    const cmsContent = cmsDiv.querySelector('.cmsContent');
-    const cmsDivsH2 = cmsDiv.querySelector('.cmsDivsH2');
-    const chevronUp = cmsDiv.querySelector('.fa-chevron-up');
-    const chevronDown = cmsDiv.querySelector('.fa-chevron-down');
-
-    cmsDivsH2.addEventListener('click', () => {
-      // Toggle the 'cmsContent' element's display property
-      if (cmsContent.style.display === 'none') {
-        cmsContent.style.display = 'block';
-        chevronUp.style.display = 'inline-block';
-        chevronDown.style.display = 'none';
-      } else {
-        cmsContent.style.display = 'none';
-        chevronUp.style.display = 'none';
-        chevronDown.style.display = 'inline-block';
-      }
-    });
-  });
-</script>
-
-<script>
-  //IBM cards tHead click tContent display none or flex
-  const tCards = document.querySelectorAll('.tCards');
-  const i2 = document.querySelector('#exploreTech .tCards .tHead .tTexts i:nth-child(2)');
-  const i3 = document.querySelector('#exploreTech .tCards .tHead .tTexts i:nth-child(3)');
-  const iSpan = document.querySelector('#exploreTech .tCards .tHead .tTexts span');
-
-  tCards.forEach((tCard) => {
-    const tHead = tCard.querySelector('.tHead');
-    const tContent = tCard.querySelector('.tContent');
-
-    tHead.addEventListener('click', () => {
-      if (tCard.classList.contains('focus-within')) {
-        tContent.style.display = 'none';
-        tCard.classList.remove('focus-within');
-      } else {
-        tContent.style.display = 'flex';
-        tCard.classList.add('focus-within');
-      }
-    });
-  });
-</script>
-
-<script>
-// Create form add more links header
-  const clinksContainers = document.querySelectorAll('.clinks-container');
-  clinksContainers.forEach(clinksContainer => {
-    const caddLinkBtn = clinksContainer.querySelector('.cadd-link');
-    const clinkInputs = clinksContainer.querySelectorAll('.clink-inputs');
-
-    caddLinkBtn.addEventListener('click', () => {
-      const cnewLinkInputs = clinkInputs[0].cloneNode(true);
-      clinksContainer.insertBefore(cnewLinkInputs, caddLinkBtn);
-    });
-  });
-</script>
-
-<script>
-// Modify form add more links 
-  const mlinksContainers = document.querySelectorAll('.mlinks-container');
-  mlinksContainers.forEach(mlinksContainer => {
-    const maddLinkBtn = mlinksContainer.querySelector('.madd-link');
-    const mlinkInputs = mlinksContainer.querySelectorAll('.mlink-inputs');
-
-    maddLinkBtn.addEventListener('click', () => {
-      const mnewLinkInputs = mlinkInputs[0].cloneNode(true);
-      mlinksContainer.insertBefore(mnewLinkInputs, maddLinkBtn);
-    });
-  });
-</script>
-
+    
+    <!-- @include('cms.headerProducts.modify')
+    @include('cms.headerServices.modify')
+    
+    @include('cms.exploreTechs.modify')
+    @include('cms.ourClients.modify')
+    @include('cms.overviews.modify') -->
--- resources/views/cms/recentWorks/modify.blade.php
+++ resources/views/cms/recentWorks/modify.blade.php
@@ -1,26 +1,66 @@
 <head>
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <script>
+    function updateHiddenInputValue(element, inputName) {
+      const inputValue = element.textContent;
+      const form = element.closest('form');
+      const hiddenInput = form.querySelector(`input[name="${inputName}"]`);
+      hiddenInput.value = inputValue;
+    }
+  </script>
+  <style>
+    button.save {
+      margin-top:1rem;
+      padding:0.5rem;
+      border:2px solid green;
+      border-radius:0.2vw;
+      color:green;
+      font-weight: 600;
+    }
+    button.save:hover {
+      color:white;
+      background-color: green;
+    }
+    button.delete {
+      margin-top:1rem;
+      padding:0.5rem;
+      border:2px solid red;
+      border-radius:0.2vw;
+      color:red;
+      font-weight: 600;
+    }
+    button.delete:hover {
+      color:white;
+      background-color: red;
+    }
+  </style>
 </head>
 
 <section id="recentWorks">
   <h2>Recent Works</h2>
   <div id="honey">
     @foreach($recentWorks as $recentWork)
-        <form class="cmsBoxDelete" action="{{ route('recentWorks.update', $recentWork->id) }}" method="POST">
-          @csrf
-          @method('PUT')
-          <input type="text" name="imgsrc" value="{{ $recentWork->imgsrc }}" class="cmsInput">
-          <input type="text" name="imgalt" value="{{ $recentWork->imgalt }}" class="cmsInput">
-          <input type="text" name="titleA" value="{{ $recentWork->titleA }}" class="cmsInput">
-          <input type="text" name="titleB" value="{{ $recentWork->titleB }}" class="cmsInput">
-          <input type="text" name="description" value="{{ $recentWork->description }}" class="cmsInput">
-
-          <div class="cmsActions">
-            <button onclick="return confirm('Are you sure you want to submit this form?')" type="submit" class="cmsButton cmsSaveButton">
-              <i class="fas fa-save"></i>
-            </button>
-          </div>
-        </form>
+    <form id="editForm" action="{{ route('recentWorks.update', $recentWork->id) }}" method="POST">
+      @csrf
+      @method('PUT')
+      <div class="rw">
+        <div class="imgL">
+          <img src="{{ $recentWork->imgsrc }}" alt="{{ $recentWork->imgalt }}" />
+          <span contenteditable="true" class="editable-titleA" oninput="updateHiddenInputValue(this, 'titleA')">{{ $recentWork->titleA }}</span>
+          <span contenteditable="true" class="editable-titleB" oninput="updateHiddenInputValue(this, 'titleB')">{{ $recentWork->titleB }}</span>
+        </div>
+        <div class="tect editable-description" contenteditable="true" oninput="updateHiddenInputValue(this, 'description')">{{ $recentWork->description }}</div>
+      </div>
+      <input type="hidden" name="imgsrc" value="{{ $recentWork->imgsrc }}">
+      <input type="hidden" name="imgalt" value="{{ $recentWork->imgalt }}">
+      <input type="hidden" name="titleA" value="{{ $recentWork->titleA }}">
+      <input type="hidden" name="titleB" value="{{ $recentWork->titleB }}">
+      <input type="hidden" name="description" value="{{ $recentWork->description }}">
+      <button class="save" type="submit">Save Changes</button>
+      <button class="delete" type="submit">Delete</button>
+    </form>
     @endforeach
   </div>
 </section>
+
+
